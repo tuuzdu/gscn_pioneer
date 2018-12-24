bed5da0646026e481bfa5de88f471c445fb743db
tuuzdu <tuuzdu@mail.ru>
2018-12-24 17:09:59 +0300

    Flight test changes

--- src/gscn_pioneer_mission/mission.py
+++ src/gscn_pioneer_mission/mission.py
@@ -2,7 +2,7 @@
 
 import rospy
 from mavros_msgs.msg import Waypoint, WaypointList
-from mavros_msgs.srv import WaypointClear, WaypointPush
+from mavros_msgs.srv import WaypointClear, WaypointPush, WaypointSetCurrent, CommandBool
 
 class Mission:
 
@@ -30,13 +30,22 @@ class Mission:
         except rospy.ServiceException as e:
             rospy.loginfo('Service call failed: {0}'.format(e))
 
-    # def arming():
-    #     rospy.wait_for_service('mavros/cmd/arming')
-    #     try:
-    #         service = rospy.ServiceProxy('mavros/cmd/arming', CommandBool)
-    #         service(True)
-    #     except rospy.ServiceException as e:
-    #         rospy.loginfo('Service call failed: {0}'.format(e))
+    def set_current(self, wp_seq):
+        rospy.wait_for_service('mavros/mission/set_current')
+        try:
+            service = rospy.ServiceProxy('mavros/mission/set_current', WaypointSetCurrent)
+            service()
+        except rospy.ServiceException as e:
+            rospy.loginfo('Service call failed: {0}'.format(e))
+
+
+    def arming(self):
+         rospy.wait_for_service('mavros/cmd/arming')
+         try:
+             service = rospy.ServiceProxy('mavros/cmd/arming', CommandBool)
+             service(True)
+         except rospy.ServiceException as e:
+             rospy.loginfo('Service call failed: {0}'.format(e))
 
     def mission_create (self):
         param = rospy.get_param('~waypoints')
@@ -44,19 +53,19 @@ class Mission:
         # rospy.logdebug(param)
 
         wl = WaypointList()
-        # wp = Waypoint()
-        # wp.frame = self.MAV_GLOBAL_FRAME
-        # wp.command = self.MAV_CMD_TAKEOFF  # takeoff
-        # wp.is_current = True
-        # wp.autocontinue = True
-        # wp.param1 = param[0]['alt']  # takeoff altitude
-        # wp.param2 = 0
-        # wp.param3 = 0
-        # wp.param4 = 0
-        # wp.x_lat = 0
-        # wp.y_long = 0
-        # wp.z_alt = param[0]['alt']
-        # wl.waypoints.append(wp)
+        wp = Waypoint()
+        wp.frame = self.MAV_GLOBAL_FRAME
+        wp.command = self.MAV_CMD_TAKEOFF  # takeoff
+        wp.is_current = True
+        wp.autocontinue = True
+        wp.param1 = param[0]['alt']  # takeoff altitude
+        wp.param2 = 0
+        wp.param3 = 0
+        wp.param4 = 0
+        wp.x_lat = 0
+        wp.y_long = 0
+        wp.z_alt = param[0]['alt']
+        wl.waypoints.append(wp)
 
         for index in range(len(param)):
             wp = Waypoint()
@@ -74,14 +83,18 @@ class Mission:
             wp.z_alt = param[index]['alt']
             wl.waypoints.append(wp)
 
-        # wp = Waypoint()
-        # wp.frame = 2 
-        # wp.command = MAV_CMD_RTL
-        # wp.is_current = False
-        # wp.autocontinue = True
-        # wl.waypoints.append(wp)
+        wp = Waypoint()
+        wp.frame = 2 
+        wp.command = MAV_CMD_RTL
+        wp.is_current = False
+        wp.autocontinue = True
+        wl.waypoints.append(wp)
+        
         rospy.loginfo('Waypoints %s', wl)
         self.push_mission(wl.waypoints)
+        self.arming()
+        rospy.sleep(2)
+        self.set_current(0)
 
         # def gps_cb(data): 
         #     lat = data.latitude
